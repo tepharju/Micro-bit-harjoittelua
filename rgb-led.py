7040a0ed222fecd9ffb0890b3957b087b9c66c48
sallikki <36477772+sallikki@users.noreply.github.com>
2018-05-06 14:48:14 +0300

    Create rgb-led.py

---
+++ rgb-led.py
@@ -0,0 +1,27 @@
+from microbit import *
+
+red = pin0
+green = pin2
+blue = pin1 
+
+while True:
+  red.write_digital(1)
+  sleep(2000) # punainen valo päälle ja odotusta 2 sekuntia
+  
+  red.write_digital(0) # Mitä tapahtuu, jos tätä riviä ei kirjoita?
+  blue.write_digital(1)
+  sleep(2000)
+  
+  # Värin asteittainen lisääminen tapahtuu seuraavasti. Mitä koodi mahtaa tehdä?
+  blue.write_digital(0)
+  sleep(1000)
+  red.write_digital(1)
+  for i in range (0, 1023):
+    green.write_analog(i)
+    sleep(10)
+  sleep(2000) 
+ 
+  # Lopuksi valot pois päältä ennen kuin while True -silmukka alkaa alusta uudelleen
+  red.write_digital(0)
+  blue.write_digital(0)
+  green.write_digital(0)
