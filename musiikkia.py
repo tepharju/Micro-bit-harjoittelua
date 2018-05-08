621adcd71b4370b3b501ae267482da7c7f6f3fa8
tepharju <36477693+tepharju@users.noreply.github.com>
2018-05-08 15:20:25 +0300

    Update musiikkia.py

--- musiikkia.py
+++ musiikkia.py
@@ -1,7 +1,7 @@
 from microbit import *
 import music # Täällä tietoa music-kirjastosta ja sävelten koodaamisesta: http://microbit-micropython.readthedocs.io/en/latest/music.html#musical-notation
 
-tune = [r:4, c4:4, d4:4, e4:4, f4:4, g4:8, g4:8, r:2, g4:4, f4:4, e4:4, d4:4, c4:8, c4:8]
+tune = ['r:4', 'c4:4', 'd4:4', 'e4:4', 'f4:4', 'g4:8', 'g4:8', 'r:2', 'g4:4', 'f4:4', 'e4:4', 'd4:4', 'c4:8', 'c4:8']
 # tune on laulu, jota soitetaan
 
 while True:
