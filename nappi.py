b2163bbd9e9373d76ad537fa7e88c814140ff0a7
Teppo Harju <36477693+tepharju@users.noreply.github.com>
2020-09-01 12:38:50 +0300

    Update nappi.py

--- nappi.py
+++ nappi.py
@@ -2,9 +2,9 @@ from microbit import *
 
 while True:
   if button_a.is_pressed():
-    pin1.write_digital(1) # pin1 "menee päälle"
+    pin0.write_digital(1) # pin0 "menee päälle"
   else:
-    pin1.write_digital(0) # "pois päältä"
+    pin0.write_digital(0) # "pois päältä"
   
 #rakenna halutessasi pin0:sta "nappi" kytkemällä sen ja gnd:n välille johto
   if pin0.is_touched(): 
